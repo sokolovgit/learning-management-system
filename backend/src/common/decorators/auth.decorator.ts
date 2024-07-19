84553e9d4111d4d9436af4195690c6f1d9adfc21
sokolovgit <sanok.sokolov928100@gmail.com>
2024-07-19 14:48:14 +0300

    style: short import path

--- backend/src/common/decorators/auth.decorator.ts
+++ backend/src/common/decorators/auth.decorator.ts
@@ -3,10 +3,7 @@ import { ApiBearerAuth } from '@nestjs/swagger';
 
 import { JwtAuthGuard } from '../guards/jwt-auth.guard';
 import { AbilitiesGuard } from '../guards/abilities.guard';
-import {
-  CheckAbilities,
-  RequiredRule,
-} from '../decorators/check-ability.decorator';
+import { CheckAbilities, RequiredRule } from './check-ability.decorator';
 
 export function Auth(...abilities: RequiredRule[]) {
   return applyDecorators(
